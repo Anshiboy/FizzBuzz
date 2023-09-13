9bd05894922b877550252dbb06dec843ae2d6b2e
ansh <ansh30704@gmail.com>
2023-09-13 17:36:46 -0400

    made it cooler

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,14 +1,14 @@
 public class Multiples {
-    public static void main(String[] args) {
+    public static int main(int n, int a, int b) {
         // Program to see how many multiples of 3 or 5 are there below 1000
         int i = 1;
         int sum = 0;
-        while (i <= 1000) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
                 sum += 1;
             }
             i++;
         }
-        System.out.println(sum);
+        return i;
     }
 }
